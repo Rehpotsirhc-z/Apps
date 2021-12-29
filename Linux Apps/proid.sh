70a07aeb548d35bc88396280635f3d310acfac0b
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2021-12-29 16:56:30 +0000

    Update Linux Apps/proid.sh

--- Linux Apps/proid.sh
+++ Linux Apps/proid.sh
@@ -2,13 +2,13 @@
 if [ "$1" = "hide" ]
 then
 	proid=$(xdo id)
-	echo $proid >> proidlog
+	echo $proid >> ~/.config/proidlog
 	xdo hide $proid
 elif [ "$1" = "show" ]
 then
-	proid=$(sed '$!d' proidlog)
+	proid=$(sed '$!d' ~/.config/proidlog)
 	xdo show $proid
-	sed -i '$d' proidlog
+	sed -i '$d' ~/.config/proidlog
 else
 	echo "No valid arguments"
 fi
