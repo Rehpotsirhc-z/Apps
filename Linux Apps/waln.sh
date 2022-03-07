782f0efef5ed272d28b89f9930224ac84f47bd2c
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2022-03-07 02:33:00 +0000

    Update Linux Apps/waln.sh

---
+++ Linux Apps/waln.sh
@@ -0,0 +1,47 @@
+#!bin/sh
+
+function waln {
+    if [ $1 == 0 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/0140.jpg"
+    fi 
+    if [ $1 == 1 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Christmas.jpg"
+    fi     
+    if [ $1 == 2 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Angel Beats.jpg"
+    fi     
+    if [ $1 == 3 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/C3.jpg"
+    fi     
+    if [ $1 == 4 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Love of Ren'ai Koutei of LOVE!.jpg"
+    fi     
+    if [ $1 == 5 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Sora no Mukou de Sakimasu you ni.jpg"
+    fi     
+    if [ $1 == 6 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Majo Koi Nikki.jpg"
+    fi     
+    if [ $1 == 7 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Innocent Girl.jpg"
+    fi     
+    if [ $1 == 8 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Witch's Garden.jpg"
+    fi     
+    if [ $1 == 9 ]
+    then 
+        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Miniskirt Pirates.jpg"
+    fi 
+
+}
+
+waln $1
