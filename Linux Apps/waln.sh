8ac050b9540d63ce98595ad79a6b1aeb540cf67c
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2022-03-12 03:55:52 +0000

    Replace waln.sh

--- Linux Apps/waln.sh
+++ Linux Apps/waln.sh
@@ -1,43 +1,43 @@
 #!bin/sh
 
 function waln {
-    if [ $1 == 0 ]
-    then 
-        nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/0140.jpg"
-    fi 
     if [ $1 == 1 ]
+    then 
+        nitrogen --restore
+    fi 
+    if [ $1 == 2 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Christmas.jpg"
     fi     
-    if [ $1 == 2 ]
+    if [ $1 == 3 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Angel Beats.jpg"
     fi     
-    if [ $1 == 3 ]
+    if [ $1 == 4 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/C3.jpg"
     fi     
-    if [ $1 == 4 ]
+    if [ $1 == 5 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Love of Ren'ai Koutei of LOVE!.jpg"
     fi     
-    if [ $1 == 5 ]
+    if [ $1 == 6 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Sora no Mukou de Sakimasu you ni.jpg"
     fi     
-    if [ $1 == 6 ]
+    if [ $1 == 7 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Majo Koi Nikki.jpg"
     fi     
-    if [ $1 == 7 ]
+    if [ $1 == 8 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Innocent Girl.jpg"
     fi     
-    if [ $1 == 8 ]
+    if [ $1 == 9 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Witch's Garden.jpg"
     fi     
-    if [ $1 == 9 ]
+    if [ $1 == 0 ]
     then 
         nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Miniskirt Pirates.jpg"
     fi 
@@ -45,3 +45,15 @@ function waln {
 }
 
 waln $1
+
+
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/DT-Wallpapers/0140.jpg" 
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Christmas.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Angel Beats.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/C3.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Love of Ren'ai Koutei of LOVE!.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Sora no Mukou de Sakimasu you ni.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Majo Koi Nikki.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Innocent Girl.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Witch's Garden.jpg"
+#nitrogen --set-zoom-fill "/home/rehpotsirhc/Nextcloud/Pictures/Anime/Miniskirt Pirates.jpg"
